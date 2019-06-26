50513e2fb87a9d46677fbef303134e9bb6fd63e5
David Pavelka <pavelka1986@gmail.com>
2019-06-26 14:24:49 +0200

    Reformat code

--- footer.php
+++ footer.php
@@ -1,24 +1,24 @@
-<div class="row row-footer">         
-    <footer id="footer" class="container">                                          
-        
-        <div class="col-md-12 colophon">                             
-            
-            <div class="col-md-4">                                      
-                <a href="<?php bloginfo('url'); ?>" class="">Home</a> | Copyright 
-                <?php echo date('Y'); ?>                                                                                              
-            </div>                            
+<div class="row row-footer">
+    <footer id="footer" class="container">
+
+        <div class="col-md-12 colophon">
+
             <div class="col-md-4">
-                Lorem ipsum                                                  
-            </div>                            
+                <a href="<?php bloginfo('url'); ?>" class="">Home</a> | Copyright
+                <?php echo date('Y'); ?>
+            </div>
+            <div class="col-md-4">
+                Lorem ipsum
+            </div>
             <div class="col-md-2">
-                Dolor dolor                                               
-            </div>                            
+                Dolor dolor
+            </div>
             <div class="col-md-2">
-                Lorem dolor                                                
-            </div> 
-                           
-        </div> 
-                                    
+                Lorem dolor
+            </div>
+
+        </div>
+
     </footer>
 </div>
 <?php wp_footer(); ?>
--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 "use strict";
-jQuery(function($){
-	
+jQuery(function ($) {
+
     var imgobj = $('a[href$=".jpg"],a[href$=".png"],a[href$=".gif"],a[href$=".JPG"],a[href$=".PNG"],a[href$=".GIF"]');
-    imgobj.attr("rel","fancybox");
+    imgobj.attr("rel", "fancybox");
     $("a[rel=fancybox]").fancybox({
-        'titleShow': true, 	
+        'titleShow': true,
     });
 }); 
 
